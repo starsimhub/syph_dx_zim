ddb540cbf4b504def9d549083b4e699c3872dd8d
Robyn Stuart <robyna.s@gmail.com>
2025-12-10 08:00:08 +1100

    do not die

--- run_calibrations.py
+++ run_calibrations.py
@@ -82,7 +82,7 @@ def make_calibration(which='hiv', n_trials=None, n_workers=None):
         data=data,
         study_name=f'{LOCATION}_{which}_calibration',
         total_trials=n_trials, n_workers=n_workers,
-        die=True, reseed=False, storage=storage, save_results=True,
+        die=False, reseed=False, storage=storage, save_results=True,
     )
 
     return sim, calib
